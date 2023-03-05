4a599e720c7bd796c75132f798a3a6666f7e5499
ordonemi <emilioordgue23@gmail.com>
2023-03-04 21:55:33 -0600

    Encapsulate Learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Fraction test2 = new Fraction(5);
+        Fraction test3 = new Fraction(3,4);
+
+
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
+        test3.SetTop(1);
+        test3.SetBottom(3);
+
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
+
     }
 }
+
