eeae58552254d86f55e56e064c888d79953a8233
ordonemi <emilioordgue23@gmail.com>
2023-03-07 12:39:10 -0600

    Inheritance Learning Activity

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string name, string topic)
+    {
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        string summary = $"{_studentName} - {_topic}";
+        return summary;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7,3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+        Console.WriteLine();
+
+
     }
 }
